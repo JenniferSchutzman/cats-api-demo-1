22a4cc0cc4f636674a0dc39830dbba076372617f
tripott <tripott@gmail.com>
2018-02-05 14:15:21 -0500

    GET /cats

--- api.js
+++ api.js
@@ -8,3 +8,12 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => res.send('meow.'))
 
 app.listen(port, () => console.log('CATS! ', port))
+
+const cats = [
+  { name: 'Mittens', age: 3, gender: 'Female', breed: 'Tabby' },
+  { name: 'Muffins', age: 2, gender: 'Male', breed: 'Siamese' },
+  { name: 'Mr. Handsome', age: 8, gender: 'Male', breed: 'Tom' },
+  { name: 'Miss Krunkles', age: 5, gender: 'Female', breed: 'Torty' }
+]
+
+app.get('/cats', (req, res) => res.send(cats))
