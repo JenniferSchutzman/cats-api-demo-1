b8a796ea7c6255f8efa9d886a46b864c386cbd96
tripott <tripott@gmail.com>
2018-02-05 15:48:14 -0500

    filter

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const app = express()
 const HTTPError = require('node-http-error')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
+const { pathOr, compose, head, last, split, filter, toLower } = require('ramda')
 
 const cats = [
   { name: 'Mittens', age: 3, gender: 'Female', breed: 'Tabby' },
@@ -13,6 +14,31 @@ const cats = [
 ]
 
 app.get('/', (req, res) => res.send('meow.'))
-app.get('/cats', (req, res) => res.send(cats))
+
+//http://localhost:5000/cats?filter=breed:Tabby
+app.get('/cats', (req, res) => {
+  // req.query.filter
+  const queryFilter = pathOr('no filter', ['query', 'filter'], req) // 'no filter' or 'breed:Tabby'
+
+  if (queryFilter === 'no filter') {
+    res.send(cats)
+  } else {
+    const searchProp = compose(head, split(':'))(queryFilter) // 'breed'
+    var searchValue = compose(last, split(':'))(queryFilter) // 'tabby'
+
+    function catFilter(cat) {
+      searchValue = isNaN(Number(searchValue))
+        ? toLower(searchValue)
+        : searchValue
+      const catValue = isNaN(Number(cat[searchProp]))
+        ? toLower(cat[searchProp])
+        : Number(cat[searchProp])
+
+      return catValue == searchValue //return true or false
+    }
+
+    res.send(filter(catFilter, cats))
+  }
+})
 
 app.listen(port, () => console.log('CATS! ', port))
